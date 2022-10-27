8d77f9b02186e27a22b6fae5830f7f22e725d1f4
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-10-27 19:16:41 -0300

    added migration files

--- NETMarket/WebApi/Program.cs
+++ NETMarket/WebApi/Program.cs
@@ -1,5 +1,7 @@
+using BusinessLogic.Data;
 using BusinessLogic.Logic;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddTransient<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MarketDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 var app = builder.Build();
