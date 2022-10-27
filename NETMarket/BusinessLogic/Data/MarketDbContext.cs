89a971a059e1bd7f3bf5de18711af199fdf179a8
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-10-27 17:59:38 -0300

    dbContext definition

---
+++ NETMarket/BusinessLogic/Data/MarketDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Data;
+
+public class MarketDbContext : DbContext
+{
+    public MarketDbContext(DbContextOptions<MarketDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Trademark> Trademarks { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
