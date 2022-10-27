3bdaad94d25cb95c9f9e5e4a1dca043cbe32b0e8
FrancoSiciliano <51268404+FrancoSiciliano@users.noreply.github.com>
2022-10-27 17:52:20 -0300

    created EF configuration

--- NETMarket/Core/Entities/Product.cs
+++ NETMarket/Core/Entities/Product.cs
@@ -2,19 +2,21 @@
 
 namespace Core.Entities;
 
-public class Product
+public class Product : BaseClass
 {
-    public int Id { get; set; }
-
-    public string Name { get; set; }
-
+    
     public string Description { get; set; }
 
     public int Stock { get; set; }
-
-    public int Trademark { get; set; }
     
-    [Column(TypeName = "decimal(18,4)")]
+    public int TrademarkId { get; set; }
+
+    public Trademark Trademark { get; set; }
+    
+    public int CategoryId { get; set; }
+    
+    public Category Category { get; set; }
+    
     public decimal Price { get; set; }
     
     public string Image { get; set; }
